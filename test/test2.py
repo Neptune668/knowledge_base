2a39f24e470cf2d7095a69c669cb85ac0184f324
Neptune668 <2935971352@qq.com>
2026-07-10 20:06:32 +0800

    test2

--- test/test2.py
+++ test/test2.py
@@ -1,6 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
 from config.mineru_config import mineru_config
 
 url = mineru_config.base_url
-api_key = mineru_config.api_key
-print(url,end="\n\n")
-print(api_key)
+token = mineru_config.api_token
+
+print(url)
+print(token)
+
