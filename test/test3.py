a062f585a573b147d528a731558a742d1c7230c6
Neptune668 <2935971352@qq.com>
2026-07-12 15:42:12 +0800

    test

--- test/test3.py
+++ test/test3.py
@@ -1,4 +1,7 @@
-
+#
 import torch
 print(torch.cuda.is_available())  # 如果输出 True，说明成功认出显卡！
 print(torch.version.cuda)
+# import torch
+# print(torch.cuda.is_available())  # 应该返回 True
+# print(torch.version.cuda)        # 应该显示 12.8
