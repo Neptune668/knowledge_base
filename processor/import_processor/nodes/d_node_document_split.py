3e0b09d4a8686910119119b2b7e1093f42d91855
Neptune668 <2935971352@qq.com>
2026-07-14 11:36:33 +0800

    Node 4 Basic Process

--- processor/import_processor/nodes/d_node_document_split.py
+++ processor/import_processor/nodes/d_node_document_split.py
@@ -10,6 +10,16 @@ class NodeDocumentSplit(BaseNode):
     name = "node_document_split"
 
     def process(self, state: ImportGraphState):
-
-
+        """
+                文档切分节点：智能文档切片
+                :param state:
+                :return:
+                """
+        # 1 参数处理
+        # 2 标题切(初切)
+        # 3 无标题兜底(默认标题)
+        # 4 块精细化处理(长切短合)
+        # 5 打印日志
+        # 6 备份
+        state["chunks"] = None
         return state
