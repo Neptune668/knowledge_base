42ad65a442e913567a304e922be0ec608c565967
Neptune668 <2935971352@qq.com>
2026-07-15 16:38:52 +0800

    E-node Step

--- processor/import_processor/nodes/e_node_item_name_recognition.py
+++ processor/import_processor/nodes/e_node_item_name_recognition.py
@@ -10,6 +10,16 @@ class NodeItemNameRecognition(BaseNode):
     name = "node_item_name_recognition"
 
     def process(self, state: ImportGraphState):
+        # 1 参数处理
 
+        # 2 上下文拼接
+
+        # 3 模型识别(总结)
+
+        # 4 回填数据(item_name - > chunks)
+
+        # 5 主体名称向量化（稠密，稀疏）
+
+        # 6 存入milvus向量库
 
         return state
