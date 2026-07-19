375c7f1e0d81267b58aa008bce253b7fd05b59a8
Neptune668 <2935971352@qq.com>
2026-07-19 09:34:51 +0800

    test

--- processor/import_processor/main_graph.py
+++ processor/import_processor/main_graph.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 
     workflow = KBImportWorkflow()
     # workflow_graph = workflow.graph
-    init_state = {"import_file_path": r"C:\Users\YuanYi\Desktop\note\0707.md"}
+    init_state = {"import_file_path": r"D:\5w.pdf"}
 
     for event in workflow.run(init_state, stream=True):
         print(f"state: {event}")
