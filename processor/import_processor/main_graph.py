56e0616e91e9eb76d9660ebc7597518f100fee8c
Neptune668 <2935971352@qq.com>
2026-07-23 09:39:58 +0800

    main

--- processor/import_processor/main_graph.py
+++ processor/import_processor/main_graph.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 
     workflow = KBImportWorkflow()
     # workflow_graph = workflow.graph
-    init_state = {"import_file_path": r"D:\test_pdf\hak180产品安全手册.pdf"}
+    init_state = {"import_file_path": r"D:\test_pdf\1.pdf"}
 
     for event in workflow.run(init_state, stream=True):
         print(f"state: {event}")
