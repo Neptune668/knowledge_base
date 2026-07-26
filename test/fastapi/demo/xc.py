eafa4c38ce9aed0059c272ecc5f0bf09f6cde0d2
Neptune668 <2935971352@qq.com>
2026-07-26 15:34:20 +0800

    coroutine

---
+++ test/fastapi/demo/xc.py
@@ -0,0 +1,23 @@
+"""
+测试查看async和同步方法的本质区别，
+结论:异步方法需要使用协程
+"""
+
+
+async def a():
+    print("a...")
+    return 2
+
+
+def b():
+    print("b...")
+    return 1
+
+
+b = b()
+print(b)
+print(type(b))
+
+a = a()
+# print(a)
+print(type(a))
