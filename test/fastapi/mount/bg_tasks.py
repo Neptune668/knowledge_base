85be2a634778b60cd37316a3885814e95a8a6dd5
Neptune668 <2935971352@qq.com>
2026-07-27 11:47:14 +0800

    backgroundTasks

---
+++ test/fastapi/mount/bg_tasks.py
@@ -0,0 +1,21 @@
+import time
+
+from fastapi import FastAPI, BackgroundTasks
+
+app = FastAPI()
+
+
+def write_log1(email: str, content: str):
+    while True:
+        print(f"正在给{email}发送信息，{content}....")
+        time.sleep(1)
+
+@app.get("/send-task/{email}")
+async def send_tasks(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_log1, email, "hello")
+    print("服务器运行。。。")
+    return {"message": "任务执行完成"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
